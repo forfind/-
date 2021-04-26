d98f344f57b2b85adcd8545fd7b80996e14b7926
forfind <2449259711@qq.com>
2021-04-26 17:14:34 +0800

    mainfile

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ import DATE_ui as uif
 yy = datetime.datetime.now().year
 mm = datetime.datetime.now().month
 
-# 获取当前年月
+
+'''
+    now()
+    函数功能：获取当前年月
+    函数参数：无
+    函数输出：无
+'''
 def now():
     global yy
     global mm
@@ -24,6 +30,12 @@ def now():
     pdcstr()
 
 
+'''
+    funl(),“lastmonth”上一月按钮触发函数
+    函数功能：计算上一月的年月，并修改全局变量的值
+    函数参数：无
+    函数输出：无
+'''
 def funl():
     global mm
     global yy
@@ -35,6 +47,12 @@ def funl():
     pdcstr()
 
 
+'''
+    funn(),“nextmonth”下一月按钮触发函数
+    函数功能：计算下一月的年月，并修改全局变量的值
+    函数参数：无
+    函数输出：无
+'''
 def funn():
     global mm
     global yy
@@ -45,11 +63,15 @@ def funn():
         yy += 1
     pdcstr()
 
-
+'''
+    fung(),“Go”按钮触发函数
+    函数功能：获取当前输入的年月，及其所对应的当月日历
+    函数参数：ui
+    函数输出：无
+'''
 def fung(ui):
     global mm
     global yy
-    print("1.yy=", yy, "mm=", mm)
     tmp_y = yy
     tmp_m = mm
     yy = int(ui.lE_y.text())
@@ -60,27 +82,33 @@ def fung(ui):
         ui.lE_y.setText(str(yy))
         ui.lE_m.setText(str(mm))
         return
-    print("2.yy=", yy, "mm=", mm)
     pdcstr()
 
-
+'''
+    dealstr(s, x, dd),
+    函数功能：检索获取的字符串，获取今天所对应日期再字符串中的标号
+    函数参数：s(string)待检索字符串,x(int)天的位数,dd(int)日期（--日）
+    函数输出：i 序号
+'''
 def dealstr(s, x, dd):
     today = str(dd)
     if x == 0:  # 显示该月日历的第一行
         return 0
     elif x == 1:            # 今天的日期’—-日‘是一位数
         for i in range(1, len(s)):
-            print("start:"+s[i]+"end")
             if (s[i - 1] == ' ' or s[i - 1] == '\n') and s[i] == today:
                 return i
     elif x == 2:            # 今天的日期’—-日‘是两位数
         for i in range(1, len(s)):
-            print(s[i], end="")
             if (s[i - 1] == ' ' or s[i - 1] == '\n') and s[i] == today[0] and s[i + 1] == today[1]:
-                print("i=",i)
                 return i
 
-
+'''
+    pdcstr()
+    函数功能：生成字符串
+    函数参数：s(string)待检索字符串,x(int)天的位数,dd(int)日期（--日）
+    函数输出：i 序号
+'''
 def pdcstr():
     global yy
     global mm
@@ -103,8 +131,6 @@ def pdcstr():
             idxtdy = dealstr(temp, 2, dd)
             if temp[idxtdy+2] == '\n':
                 ui.textBrowser.setText(temp[0:idxtdy-1]+'['+today+']'+temp[idxtdy+2:])
-            elif temp[idxtdy-2] == '\n':
-                ui.textBrowser.setText(temp[0:idxtdy-1]+'['+today+']'+temp[idxtdy+3:])
             else:
                 ui.textBrowser.setText(temp[0:idxtdy-1]+'['+today+']'+temp[idxtdy+3:])
     else:
@@ -113,15 +139,21 @@ def pdcstr():
 
 
 if __name__ == '__main__':
+    # 初始化
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
     ui = uif.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    # 连接按钮gbtn与函数fung，并传参数ui
     ui.gbtn.clicked.connect(partial(fung, ui))
+    # 连接按钮lbtn与函数funl
     ui.lbtn.clicked.connect(funl)
+    # 连接按钮nbtn与函数funn
     ui.nbtn.clicked.connect(funn)
+    # 连接按钮nowbtn与函数now
     ui.nowbtn.clicked.connect(now)
-
+    #显示当月日历
     pdcstr()
+    # 退出程序
     sys.exit(app.exec_())
